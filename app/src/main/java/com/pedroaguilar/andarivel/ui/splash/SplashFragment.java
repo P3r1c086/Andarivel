34b48be8ff7e39736406b28a7b0be1cb3522af6c
P3r1c086 <80258593+P3r1c086@users.noreply.github.com>
2022-05-06 16:00:14 +0200

    Comentarios

--- app/src/main/java/com/pedroaguilar/andarivel/ui/splash/SplashFragment.java
+++ app/src/main/java/com/pedroaguilar/andarivel/ui/splash/SplashFragment.java
@@ -19,6 +19,13 @@ import com.pedroaguilar.andarivel.R;
  */
 public class SplashFragment extends Fragment {
 
+    /**
+     * Aquí se crea la vista, inflando el layout fragment.splash
+     * @param inflater
+     * @param container
+     * @param savedInstanceState
+     * @return un objeto inlater el cual infla el fragmento
+     */
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -26,17 +33,32 @@ public class SplashFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_splash, container, false);
     }
 
+    /**
+     * Aquí se realiza la logica una vez inflada la vista en el metodo anterior
+     * @param view
+     * @param savedInstanceState
+     */
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        long time = 2000L;
+        /**
+         * Objeto Handler (manipulador), permite enviar y procesar objetos mensaje ejecutables. Esta asociado al objeto Runnable el cual iniciara el proceso,
+         * en este caso lanza el proceso action_splash_to_login, el cual navega desde el splash hacia el login despues de un tiempo de 2 segundos.
+         */
+        long time = 2000L;//2000 milisegundos = 2 segundos
         final Handler handler = new Handler();
         Runnable r = new Runnable() {
             public void run() {
+                /**
+                 * Este objeto Navigation es un singleton, es decir, solo se puede crear una instacia de el.
+                 */
                 Navigation.findNavController(view).navigate(R.id.action_splash_to_login);
             }
         };
+        /**
+         * El método postDelayed, hace que Runnable r se agregue a la cola de mensajes, para que se ejecute después de que transcurra la cantidad de tiempo especificada.
+         */
         handler.postDelayed(r, time);
     }
 }
