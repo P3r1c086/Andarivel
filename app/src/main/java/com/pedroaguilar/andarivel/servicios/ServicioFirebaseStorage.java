79c474601f926719ae8682e96b5b40c174c3ca58
P3r1c086 <80258593+P3r1c086@users.noreply.github.com>
2022-05-25 20:19:42 +0200

    Añadido servicio de firebaseStorage management

---
+++ app/src/main/java/com/pedroaguilar/andarivel/servicios/ServicioFirebaseStorage.java
@@ -0,0 +1,25 @@
+package com.pedroaguilar.andarivel.servicios;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+public class ServicioFirebaseStorage {
+
+    FirebaseStorage storage = FirebaseStorage.getInstance();
+    StorageReference storageReference = storage.getReference();
+
+    public void guardarImagenDePerfil(String userID, byte[] data, OnFailureListener failureListener, OnCompleteListener<UploadTask.TaskSnapshot> onCompleteListener){
+        UploadTask uploadTask = storageReference.child("imagenesPerfil/" + userID +".jpg").putBytes(data);
+        uploadTask.addOnFailureListener(failureListener).addOnCompleteListener(onCompleteListener);
+    }
+
+    public StorageReference getUserPerfilUrl(String userID){
+        return storage.getReferenceFromUrl("gs://andarivel-ficha.appspot.com/imagenesPerfil/" + userID +".jpg");
+    }
+
+
+}
