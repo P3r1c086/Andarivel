6c3cceb45a043208c170bb8c8e912b8befe12ec9
P3r1c086 <80258593+P3r1c086@users.noreply.github.com>
2022-06-02 20:46:31 +0200

    Limpieza de codigo

--- app/src/main/java/com/pedroaguilar/andarivel/ui/panelAdministrador/ausencias/AusenciasFragment.java
+++ app/src/main/java/com/pedroaguilar/andarivel/ui/panelAdministrador/ausencias/AusenciasFragment.java
@@ -57,11 +57,6 @@ public class AusenciasFragment extends Fragment implements TabLayoutMediator.Tab
                 }
             }
         });
-
-
-
-
-
     }
 
     public void setViewPagerAdapter(Boolean esAdmin){
@@ -88,115 +83,5 @@ public class AusenciasFragment extends Fragment implements TabLayoutMediator.Tab
     }
 }
 
-/*
-    public AusenciasFragment() {
-        // Required empty public constructor
-    }
 
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-
-        binding = FragmentAusenciasBinding.inflate(inflater, container, false);
-        return binding.getRoot();
-    }
-
-
-    @Override
-    public void onViewCreated(View view, Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-
-
-        //Asociar un evento de clic al EditText
-        binding.etFechaInicio.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                showFechaInicioDatePickerDialog();
-            }
-        });
-        //Asociar un evento de clic al EditText
-        binding.etFechaFin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                showFechaFinDatePickerDialog();
-            }
-        });
-        setListeners();
-
-
-    }*/
-
-
-  /*  private void setListeners(){
-        binding.spMotivoAusencia.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                String elementoSeleccionado = (String) parent.getItemAtPosition(position);
-                usuario.setMotivoAusencia(elementoSeleccionado);
-                //todo:falta pasarlo a la base de datos
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {}
-        });
-        binding.btSolicitar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(binding.etDescripcion.getText().toString().isEmpty()){
-                    usuario.setDescripcionAusencia("No especificado");
-                }else {
-                    usuario.setDescripcionAusencia(binding.etDescripcion.getText().toString());
-                }
-                //todo:falta pasarlo a la base de datos
-                introducirDatosEnBd();
-            }
-        });
-    }
-
-    /**
-     * Mostrar el DatePicker en un dialog
-     */
-   /* private void showFechaInicioDatePickerDialog() {
-        final Calendar c = Calendar.getInstance();
-        new DatePickerDialog(getContext(), new DatePickerDialog.OnDateSetListener() {
-            @Override
-            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
-                // +1 because January is zero
-                final String selectedDateInicio = day + " / " + (month+1) + " / " + year;
-                binding.etFechaInicio.setText(selectedDateInicio);
-                usuario.setFechaInicioAusencia(selectedDateInicio);
-                //almacenarFechaInicio();
-            }
-        }, c.get(Calendar.YEAR), c.get(Calendar.MONTH),  c.get(Calendar.DAY_OF_MONTH)).show();
-    }
-
-    /**
-     * Mostrar el DatePicker en un dialog
-     */
-   /* private void showFechaFinDatePickerDialog() {
-        final Calendar c = Calendar.getInstance();
-        new DatePickerDialog(getContext(), new DatePickerDialog.OnDateSetListener() {
-            @Override
-            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
-                // +1 because January is zero
-                final String selectedDateFin = day + " / " + (month+1) + " / " + year;
-                binding.etFechaFin.setText(selectedDateFin);
-                usuario.setFechaFinAusencia(selectedDateFin);
-                //almacenarFechaFin();
-            }
-        }, c.get(Calendar.YEAR), c.get(Calendar.MONTH),  c.get(Calendar.DAY_OF_MONTH)).show();
-    }
-
-    private void introducirDatosEnBd(){
-        Usuario user = new Usuario();
-        user.setID(mAuth.getCurrentUser().getUid());
-        user.setMotivoAusencia(usuario.getMotivoAusencia());
-        user.setFechaInicioAusencia(usuario.getFechaInicioAusencia());
-        user.setFechaFinAusencia(usuario.getFechaFinAusencia());
-        user.setDescripcionAusencia(usuario.getDescripcionAusencia());
-        databaseReference.child(Constantes.TABLA_AUSENCIAS).child(user.getID()).setValue(user);
-    }*/
 
