037e88be6dbc287579ce3681d8c1c2b016105202
pajar <pajaros33@gmail.com>
2022-12-02 17:11:51 +0100

    logica para envio de mensajes empezada

--- app/src/main/java/com/pedroaguilar/andarivel/servicios/MiServiciodeFirebaseMessaging.java
+++ app/src/main/java/com/pedroaguilar/andarivel/servicios/MiServiciodeFirebaseMessaging.java
@@ -9,19 +9,26 @@ import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Build;
 import android.util.Log;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.core.app.NotificationCompat;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 import com.pedroaguilar.andarivel.R;
 import com.pedroaguilar.andarivel.modelo.TipoNotificacion;
 import com.pedroaguilar.andarivel.presentacion.ui.login.LoginActivity;
 
+import java.util.Map;
+
 public class MiServiciodeFirebaseMessaging extends FirebaseMessagingService {
 
     public static final String TAG = "MiServicioDeFirebase";
+
+    private final FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+    private final ServicioFirebaseDatabase database = new ServicioFirebaseDatabase();
 
     @Override
     public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
@@ -32,6 +39,22 @@ public class MiServiciodeFirebaseMessaging extends FirebaseMessagingService {
             Log.d(TAG, "Message data payload: " + remoteMessage.getData());
             if (remoteMessage.getData().get("TipoNotificacion") != null && remoteMessage.getData().get("TipoNotificacion").equals(TipoNotificacion.RECORDATORIO.name())) {
                 //Todo Consulta a los datos del usuario para ver si tiene un nodo fichaje correspondiente al día actual
+                database.getFichajesUser(firebaseAuth.getUid(), task -> {
+                    if (task.isSuccessful()) {
+                        //Accedemos al mapa fichajes del nodo del usuario
+                        Map<String, Object> fichajes = (Map<String, Object>) ((Map<String, Object>) task.getResult().getValue()).get("Fichajes");
+                        if (fichajes != null) {
+                            //Nos guardamos las referencias de los fichajes del usuario
+                            for (String key : fichajes.keySet()) {
+                                fichajes.put(key, null);
+                            }
+                            //todo: ahora que tengo una lista de fichajes del usuario, hay que comprobar si el fichaje pertenece al día actual
+                            Log.i("fichajes", fichajes.toString());
+                        } else {
+                            Toast.makeText(this, "Este usuario no tiene fichajes", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
                 //Todo Si no tiene el nodo; creamos la notificacion, si sí lo tiene; nada
                 sendNotification(
                         remoteMessage.getData().get("Title"),
